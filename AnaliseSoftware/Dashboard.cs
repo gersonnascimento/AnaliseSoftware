7b542332445b4fcc7c46322e46c5e080e2ab01b4
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-05-15 23:46:41 -0300

    PrimeiraVersao

---
+++ AnaliseSoftware/Dashboard.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AnaliseSoftware
+{
+    public partial class Dashboard : Form
+    {
+        public int usu;
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAvaliar_Click(object sender, EventArgs e)
+        {
+
+            FrmCadastroSoftware a = new FrmCadastroSoftware();
+            a.usu = this.usu;
+            
+            a.Visible = true;
+            this.Hide();
+
+           
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            FrmCadastroSoftware frmCadastroSoftware = new FrmCadastroSoftware();
+            this.Hide();
+            frmCadastroSoftware.Visible = true;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnMeusDados_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAvaliar_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void btnAvaliar_MouseHover(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+
+            Hide();     
+        }
+
+        private void btnRanking_Click(object sender, EventArgs e)
+        {
+            Ranking ranking = new Ranking();
+            ranking.Show();
+        }
+    }
+}
---
+++ AnaliseSoftware/Ranking.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AnaliseSoftware
+{
+    public partial class Ranking : Form
+    {
+        public int[] vetorSoftware;
+        public Ranking()
+        {
+            InitializeComponent();
+        }
+
+        public void formaVetor()
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;uid=root;pwd='';database=dbanalise");
+
+
+            String sql = "select * from softwares";
+
+
+            MySqlCommand cnn = new MySqlCommand(sql, conn);
+
+            conn.Open();
+
+            MySqlDataReader dr = cnn.ExecuteReader();
+            int contalinha = 0;
+            while (dr.Read())
+            {
+                contalinha++;
+            }
+            conn.Close();
+            MySqlCommand CNN = new MySqlCommand(sql, conn);
+            conn.Open();
+            MySqlDataReader comando = cnn.ExecuteReader();
+            vetorSoftware = new int[contalinha];
+
+            while (comando.Read())
+            {
+                // monta o array de valores...
+                object[] values = new object[comando.FieldCount];
+
+                // adiciona as colunas no grid...
+                if (dataGridView1.Rows.Count == 0)
+                    for (int i = 0; i < comando.FieldCount; i++)
+                        dataGridView1.Columns.Add(comando.GetName(i), comando.GetName(i));
+
+                // varre as colunas para preencher os valores...
+                for (int i = 0; i < comando.FieldCount; i++)
+                {
+                    values[i] = comando.GetValue(i);
+                }
+                for (int i = 0; i < comando.FieldCount; i += 3)
+                {
+                    vetorSoftware[i] = Convert.ToInt16(comando.GetValue(i));
+                    MessageBox.Show("Adicionou: " + vetorSoftware[i]);
+                }
+
+
+                for (int i = 0; i < comando.FieldCount; i += 3)
+                {
+                    string it = comando.GetValue(i).ToString();
+                    // MessageBox.Show(Convert.ToString(dr.FieldCount));
+                    //   vetor[i] = dr.GetValue(i);
+                }
+
+                // adiciona no grid...
+                dataGridView1.Rows.Add(values);
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           formaVetor();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Software software = new Software(0,null,0);
+            software.pegaMedia(2);
+        }
+    }
+}
